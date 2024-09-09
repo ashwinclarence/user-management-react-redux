e81030e92b1db316cee151cc48bdb685ebff798c
ASHWIN <meashwinclarence@gmail.com>
2024-09-09 19:54:35 +0530

    Fea: server created using express

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+    
+})
