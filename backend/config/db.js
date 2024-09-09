4438be911a66a27dbc30418a35bff3c9d3345ffe
ASHWIN <meashwinclarence@gmail.com>
2024-09-09 20:02:58 +0530

    fea: mongodb connection using mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+
+        if (connect) {
+            console.log("mongoose connected");
+            
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db';
 
 const app = express();
 dotenv.config();
 
+app.use(express.json())
 
+// mongodb connection
+connectDB();
+
+// port for running the server
 const port = process.env.PORT || 3000;
 
 
